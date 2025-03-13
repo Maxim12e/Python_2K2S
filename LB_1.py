bd11c166b6326f59039a479ca359d62a52a50999
Maksim <122976125+Maxim12e@users.noreply.github.com>
2025-03-13 18:41:17 +0200

    Create LB_1.py

---
+++ LB_1.py
@@ -0,0 +1,28 @@
+import re
+
+while True:
+    author = input("author: ")
+    if re.search(r'\d', author):
+        print('ERROR WITH NAME')
+    else:
+        break
+
+book = input("book: ")
+
+
+
+Book_dict = {}
+
+Book_dict = {
+    "Author" : "Robert Shekli",
+    "Name_book" : "Leech"
+}
+
+Book_dict[book] = author
+
+#for values in Book_dict.values():
+#   print(values)
+
+
+print(Book_dict)
+
